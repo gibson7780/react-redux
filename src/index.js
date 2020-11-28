1914a9090a76f8d677af23674b5d3f80e7de7b11
nickpeng <gibson7780@champtron.com>
2020-11-28 23:48:02 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+class ConnectTitle extends React.Component {
+  render() {
+    return <h1>Hello!{this.props.name}</h1>;
+  }
+}
+
+const initState = {
+  name: 'Nick',
+};
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const mapStateToProps = (state) => ({
+  name: state.name,
+});
+
+const Title = connect(mapStateToProps)(ConnectTitle);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Title />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
