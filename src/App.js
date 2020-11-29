f5296101926bf6704a9322769826bc84c522e1c3
nickpeng <gibson7780@champtron.com>
2020-11-30 01:49:32 +0800

    todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import './App.css';
+import { useState } from 'react';
+const App = (props) => {
+  console.log('props', props);
+  const [todo, setTodo] = useState('');
 
-function App() {
-  return <h1>Hello!</h1>;
-}
+  return (
+    <div>
+      {/* <h1>Hello!{props.name}!</h1> */}
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        onKeyUp={(e) => {
+          console.log(e.key);
+          if (e.key === 'Enter') {
+            setTodo('');
+            props.addTodo(todo);
+          }
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodo('');
+          props.addTodo(todo);
+        }}
+      >
+        ADD
+      </button>
+
+      <div>
+        <ul>
+          {props.todoList.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,29 +5,52 @@ import App from './App';
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
 
-class ConnectTitle extends React.Component {
-  render() {
-    return <h1>Hello!{this.props.name}</h1>;
-  }
-}
+// class ConnectTitle extends React.Component {
+//   render() {
+//     return <h1>Hello!{this.props.name}</h1>;
+//   }
+// }
 
 const initState = {
-  name: 'Nick',
+  todoList: [],
 };
+
+const ADD_TODOLIST = 'ADD_TODOLIST';
+
 const reducer = (state = initState, action) => {
   switch (action.type) {
+    case ADD_TODOLIST:
+      return {
+        todoList: [...state.todoList, action.payload.todo],
+      };
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
+console.log('store', store);
+
+const addTodo = (todo) => {
+  return {
+    type: ADD_TODOLIST,
+    payload: {
+      todo: todo,
+    },
+  };
+};
 
 const mapStateToProps = (state) => ({
-  name: state.name,
+  todoList: state.todoList,
 });
 
-const Title = connect(mapStateToProps)(ConnectTitle);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (todo) => dispatch(addTodo(todo)),
+  };
+};
+
+const Title = connect(mapStateToProps, mapDispatchToProps)(App);
 
 ReactDOM.render(
   <Provider store={store}>
